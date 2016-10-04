e55de62470a9ab1d0b7dd74510d82556573fb4b8
Gabrielle <gat2118@columbia.edu>
2016-10-04 12:25:43 -0400

    first commit

---
+++ authentication/urls.py
@@ -0,0 +1,7 @@
+# authentication/urls.py
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.home, name='home'),
+]
---
+++ stocks/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+# Create your models here.
+
+@python_2_unicode_compatible
+class User(models.Model):
+	username = models.CharField(max_length=80)
+	password = models.CharField(max_length=80)
+	def __str__(self):
+		return self.username
+
+@python_2_unicode_compatible
+class ParentOrder(models.Model):
+	quantity = models.IntegerField(default=0)
+	stock_type = models.CharField(max_length=20)
+	is_sell = models.BooleanField(default=True)
+	time_executed = models.DateTimeField(auto_now=False, auto_now_add=True)
+	order_id = models.AutoField(primary_key=True)
+	def __str__(self):
+		return self.order_id
+
+@python_2_unicode_compatible
+class ChildOrder(models.Model):
+	order_id =models.ForeignKey(ParentOrder, on_delete=models.CASCADE)
+	quantity = models.IntegerField(default=0)
+	is_successful = models.BooleanField(default=False)
+	price = models.FloatField(default=0.0)
+	def __str__(self):
+		return self.id
