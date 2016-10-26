4582cea9e2209c0d2f194828f1c5764c86f14b3b
Leon Song <ls3233@columbia.edu>
2016-10-25 22:30:00 -0400

    leons changes to models

--- stocks/models.py
+++ stocks/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.contrib.auth.models import User
+import urllib2
+import time
+import json
+
+
+QUERY = "http://localhost:8080/query?id={}"
+ORDER = "http://localhost:8080/order?id={}&side=sell&qty={}&price={}"
+ORDER_DISCOUNT = 10
+N = 5
 
 # Create your models here.
 @python_2_unicode_compatible
@@ -21,6 +30,22 @@ class ParentOrder(models.Model):
 
 	def __str__(self):
 		return str(self.id) + ": " + str(self.quantity) + " x " + self.stock_type
+
+	def query_market_price(self):
+		quote = json.loads(urllib2.urlopen(QUERY.format(self.id)).read())
+		price = float(quote['top_bid']['price'])
+		print "Quoted at %s" % price
+		return price
+
+	def trade(self):
+		number_of_successes = 0
+		maximum_child_size = self.quantity / 10  # hardcoded to split by 10%
+
+		
+
+
+
+
 
 @python_2_unicode_compatible
 class ChildOrder(models.Model):
