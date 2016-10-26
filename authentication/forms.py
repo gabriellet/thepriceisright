3b623feb3b610a7aa6ab365532ea1006bb48c189
Gabrielle <gat2118@columbia.edu>
2016-10-25 23:46:14 -0400

    styling with bootstrap

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 from django import forms
 
 class LoginForm(AuthenticationForm):
-	username = forms.CharField(label="Username", max_length=30, 
-		widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
-	password = forms.CharField(label="Password", max_length=30, 
-		widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
+	username = forms.CharField(label="Username", 
+		widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username', 'placeholder': 'Username', 'autofocus': 'autofocus'}))
+	password = forms.CharField(label="Password", 
+		widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password', 'placeholder': 'Password', 'autofocus': 'autofocus'}))
 
 
 # class UserForm(forms.ModelForm):
