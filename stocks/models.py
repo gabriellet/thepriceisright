b7509b224dc2adb9a609cad0ad1c0613a05bb7a8
Jackie Lin <jackielin13@gmail.com>
2016-12-15 19:51:15 -0500

    style changes and comments

--- stocks/models.py
+++ stocks/models.py
@@ -19,223 +19,223 @@ SUCCESS_TOLERANCE = 2
 # Create your models here.
 @python_2_unicode_compatible
 class ParentOrder(models.Model):
-	quantity = models.IntegerField(blank=False, default=1)
-	stock_type = models.CharField(max_length=20)
-	is_sell = models.BooleanField(blank=False, default=True)
-	time_executed = models.DateTimeField(blank=False, auto_now_add=True)
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField(blank=False, default=1)
+    stock_type = models.CharField(max_length=20)
+    is_sell = models.BooleanField(blank=False, default=True)
+    time_executed = models.DateTimeField(blank=False, auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-	IN_PROGRESS = 'P'
-	COMPLETED = 'C'
-	FAILED = 'F'
-	PAUSED = 'S'
-	CANCELLED = 'X'
-	STATUS_CHOICES = (
-		(IN_PROGRESS, 'In Progress'),
-		(COMPLETED, 'Completed'),
-		(FAILED, 'Failed'),
-		(PAUSED, 'Paused'),
-		(CANCELLED, 'Cancelled')	
-	)
+    IN_PROGRESS = 'P'
+    COMPLETED = 'C'
+    FAILED = 'F'
+    PAUSED = 'S'
+    CANCELLED = 'X'
+    STATUS_CHOICES = (
+        (IN_PROGRESS, 'In Progress'),
+        (COMPLETED, 'Completed'),
+        (FAILED, 'Failed'),
+        (PAUSED, 'Paused'),
+        (CANCELLED, 'Cancelled')    
+    )
 
-	status = models.CharField(
-		max_length=1,
-		choices=STATUS_CHOICES,
-		default=IN_PROGRESS
-	)
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_CHOICES,
+        default=IN_PROGRESS
+    )
 
-	progress = models.FloatField(blank=False, default=0.0)
-	
-	# checks if quantity is positive
-	def is_valid(self):
-		if self.quantity <= 0:
-			return False
-		else:
-			return True
+    progress = models.FloatField(blank=False, default=0.0)
+    
+    # checks if quantity is positive
+    def is_valid(self):
+        if self.quantity <= 0:
+            return False
+        else:
+            return True
 
-	def get_total_price_sold(self, children):
-		total_price = (children.filter(is_successful=True).aggregate(total=Sum(F('price') * F('quantity'))))['total']
-		if total_price is None:
-			return 0.0
-		else:
-			return total_price
+    def get_total_price_sold(self, children):
+        total_price = (children.filter(is_successful=True).aggregate(total=Sum(F('price') * F('quantity'))))['total']
+        if total_price is None:
+            return 0.0
+        else:
+            return total_price
 
-	def get_total_quantity_sold(self, children):
-		total_quantity = (children.filter(is_successful=True).aggregate(Sum('quantity')))['quantity__sum']
-		if total_quantity is None:
-			return 0
-		else:
-			return total_quantity
+    def get_total_quantity_sold(self, children):
+        total_quantity = (children.filter(is_successful=True).aggregate(Sum('quantity')))['quantity__sum']
+        if total_quantity is None:
+            return 0
+        else:
+            return total_quantity
 
-	def get_stats(self, children):
-		if len(children) == 0:
-			return {'total_price': 0.0, 'total_quantity': 0, 'average_price': 0.0}
-		
-		total_price = self.get_total_price_sold(children)
-		total_quantity = self.get_total_quantity_sold(children)
-		if total_price == 0.0:
-			return {'total_price': total_price, 'total_quantity': total_quantity, 'average_price': 0.0}
-		else:
-			return {'total_price': total_price, 'total_quantity': total_quantity, 'average_price': total_price/total_quantity}
+    def get_stats(self, children):
+        if len(children) == 0:
+            return {'total_price': 0.0, 'total_quantity': 0, 'average_price': 0.0}
+        
+        total_price = self.get_total_price_sold(children)
+        total_quantity = self.get_total_quantity_sold(children)
+        if total_price == 0.0:
+            return {'total_price': total_price, 'total_quantity': total_quantity, 'average_price': 0.0}
+        else:
+            return {'total_price': total_price, 'total_quantity': total_quantity, 'average_price': total_price/total_quantity}
 
-	def update_progress(self):
-		children = ChildOrder.objects.filter(parent_order__id=self.id)
-		quantity_sold = self.get_total_quantity_sold(children)
+    def update_progress(self):
+        children = ChildOrder.objects.filter(parent_order__id=self.id)
+        quantity_sold = self.get_total_quantity_sold(children)
 
-		self.progress = (float(quantity_sold)/float(self.quantity)) * 100
-		self.save()
+        self.progress = (float(quantity_sold)/float(self.quantity)) * 100
+        self.save()
 
-	def verify_market_price(self, price_json):
-		top_bid = price_json.get('top_bid')
-		if top_bid is None:
-			return False
-		price = top_bid.get('price')
-		if price is None:
-			return False
-		return True
+    def verify_market_price(self, price_json):
+        top_bid = price_json.get('top_bid')
+        if top_bid is None:
+            return False
+        price = top_bid.get('price')
+        if price is None:
+            return False
+        return True
 
-	def query_market_price(self, number_of_tries=10):
-		quote = json.loads(urllib2.urlopen(QUERY.format(self.id)).read())
-		while not self.verify_market_price(quote):  # We failed to verify, the exchange simulator returned a bad response
-			quote = json.loads(urllib2.urlopen(QUERY.format(self.id)).read())
-			time.sleep(5)  # sleep 5 seconds before trying again
-			number_of_tries -= 1
-			if number_of_tries == 0:  # no tries left, we give up querying and selling
-				return False
-		price = float(quote['top_bid']['price'])
-		print "Quoted at %s" % price
-		return price
+    def query_market_price(self, number_of_tries=10):
+        quote = json.loads(urllib2.urlopen(QUERY.format(self.id)).read())
+        while not self.verify_market_price(quote):  # We failed to verify, the exchange simulator returned a bad response
+            quote = json.loads(urllib2.urlopen(QUERY.format(self.id)).read())
+            time.sleep(5)  # sleep 5 seconds before trying again
+            number_of_tries -= 1
+            if number_of_tries == 0:  # no tries left, we give up querying and selling
+                return False
+        price = float(quote['top_bid']['price'])
+        print "Quoted at %s" % price
+        return price
 
-	def query_market_datetime(self, number_of_tries=10):
-		quote = json.loads(urllib2.urlopen(QUERY.format(self.id)).read())
-		# do we need this while loop?
-		while not self.verify_market_price(quote):  # We failed to verify, the exchange simulator returned a bad response
-			quote = json.loads(urllib2.urlopen(QUERY.format(self.id)).read())
-			time.sleep(5)  # sleep 5 seconds before trying again
-			number_of_tries -= 1
-			if number_of_tries == 0:  # no tries left, we give up querying and selling
-				return False
-		d = quote['timestamp'] + " UTC" # add UTC since this is time zone returned by simulator
-		FORMAT = '%Y-%m-%d %H:%M:%S.%f %Z'
-		dt = datetime.datetime.strptime(d, FORMAT)
-		return dt
+    def query_market_datetime(self, number_of_tries=10):
+        quote = json.loads(urllib2.urlopen(QUERY.format(self.id)).read())
+        # do we need this while loop?
+        while not self.verify_market_price(quote):  # We failed to verify, the exchange simulator returned a bad response
+            quote = json.loads(urllib2.urlopen(QUERY.format(self.id)).read())
+            time.sleep(5)  # sleep 5 seconds before trying again
+            number_of_tries -= 1
+            if number_of_tries == 0:  # no tries left, we give up querying and selling
+                return False
+        d = quote['timestamp'] + " UTC" # add UTC since this is time zone returned by simulator
+        FORMAT = '%Y-%m-%d %H:%M:%S.%f %Z'
+        dt = datetime.datetime.strptime(d, FORMAT)
+        return dt
 
-	def execute_sell(self, quantity, price):
-		url   = ORDER.format(self.id, quantity, price)
-		response = urllib2.urlopen(url).read()
-		if response == "":
-			order = {
-			u'timestamp': self.query_market_datetime(),
-			u'qty': quantity, 
-			u'side': u'sell', 
-			u'avg_price': 0.0
-			}
-		else:
-			order = json.loads(response)
-		return order
+    def execute_sell(self, quantity, price):
+        url   = ORDER.format(self.id, quantity, price)
+        response = urllib2.urlopen(url).read()
+        if response == "":
+            order = {
+            u'timestamp': self.query_market_datetime(),
+            u'qty': quantity, 
+            u'side': u'sell', 
+            u'avg_price': 0.0
+            }
+        else:
+            order = json.loads(response)
+        return order
 
-	def create_child(self, order, attempted_price):
-		if (order['avg_price'] == 0.0):
-			co = ChildOrder.objects.create(
-				parent_order=self, 
-				quantity = order['qty'],
-				is_successful=False, 
-				price=attempted_price
-			)
-		else:
-			co = ChildOrder.objects.create(
-				parent_order=self, 
-				quantity = order['qty'],
-				is_successful=True, 
-				price=order['avg_price']
-			)
-			self.progress += (order['qty']/self.quantity) * 100
-			self.save()
+    def create_child(self, order, attempted_price):
+        if (order['avg_price'] == 0.0):
+            co = ChildOrder.objects.create(
+                parent_order=self, 
+                quantity = order['qty'],
+                is_successful=False, 
+                price=attempted_price
+            )
+        else:
+            co = ChildOrder.objects.create(
+                parent_order=self, 
+                quantity = order['qty'],
+                is_successful=True, 
+                price=order['avg_price']
+            )
+            self.progress += (order['qty']/self.quantity) * 100
+            self.save()
 
-			# self.update_progress()
-			# print "==========="
-			# print "PROGRESS"
-			# print self.progress
-			# print "==========="
+            # self.update_progress()
+            # print "==========="
+            # print "PROGRESS"
+            # print self.progress
+            # print "==========="
 
-		co.time_executed = order['timestamp']
-		co.save()
-		return co
+        co.time_executed = order['timestamp']
+        co.save()
+        return co
 
-	def determine_quantity_to_sell(self):
-		successful_children = ChildOrder.objects.filter(parent_order=self.id).filter(is_successful=True)
-		quantity_sold_so_far = 0
-		for child in successful_children:
-			quantity_sold_so_far += child.quantity
-		quantity_left_to_sell = self.quantity - quantity_sold_so_far
-		return quantity_left_to_sell
+    def determine_quantity_to_sell(self):
+        successful_children = ChildOrder.objects.filter(parent_order=self.id).filter(is_successful=True)
+        quantity_sold_so_far = 0
+        for child in successful_children:
+            quantity_sold_so_far += child.quantity
+        quantity_left_to_sell = self.quantity - quantity_sold_so_far
+        return quantity_left_to_sell
 
-	def trade(self):
-		if not self.is_valid():
-			print "Problem, someone created a bad parent order that shouldn't even be here"
-			return  # This code should never be reached, since if an order is not valid, we don't even create it
+    def trade(self):
+        if not self.is_valid():
+            print "Problem, someone created a bad parent order that shouldn't even be here"
+            return  # This code should never be reached, since if an order is not valid, we don't even create it
 
-		number_of_successes = 0
-		if ((self.quantity/10) < 10):
-			maximum_child_size = self.quantity
-		else:
-			maximum_child_size = self.quantity / 10  # hardcoded to split by 10%
-		child_order_size = maximum_child_size
-		quantity_to_sell = self.determine_quantity_to_sell()
+        number_of_successes = 0
+        if ((self.quantity/10) < 10):
+            maximum_child_size = self.quantity
+        else:
+            maximum_child_size = self.quantity / 10  # hardcoded to split by 10%
+        child_order_size = maximum_child_size
+        quantity_to_sell = self.determine_quantity_to_sell()
 
-		while quantity_to_sell > 0:
-			price = self.query_market_price()
-			if price is False:
-				self.status = self.FAILED
-				self.save()
-				return
-			price -= ORDER_DISCOUNT  # query is successful, update price accordingly
-			if (quantity_to_sell < child_order_size):
-				child_order_size = quantity_to_sell  # make sure child order size never exceeds what we have left to sell
+        while quantity_to_sell > 0:
+            price = self.query_market_price()
+            if price is False:
+                self.status = self.FAILED
+                self.save()
+                return
+            price -= ORDER_DISCOUNT  # query is successful, update price accordingly
+            if (quantity_to_sell < child_order_size):
+                child_order_size = quantity_to_sell  # make sure child order size never exceeds what we have left to sell
 
-			self.refresh_from_db()  # refresh before checking if paused or cancelled
-			if (self.status == self.PAUSED or self.status == self.CANCELLED):
-				return
-				
-			order = self.execute_sell(child_order_size, price)
-			self.create_child(order, price)
+            self.refresh_from_db()  # refresh before checking if paused or cancelled
+            if (self.status == self.PAUSED or self.status == self.CANCELLED):
+                return
+                
+            order = self.execute_sell(child_order_size, price)
+            self.create_child(order, price)
 
-			# logic for handling failure to sell
-			if (order['avg_price'] == 0.0):
-				number_of_successes -= 1
-			else:
-				number_of_successes += 1
-				# update quantity if successful
-				quantity_to_sell -= order['qty']
-			
-			# Divide child order quantity into smaller chunks if
-			# we hit the failure tolerance
-			if (number_of_successes == FAILURE_TOLERANCE):
-				child_order_size /= 2
-				number_of_successes = 0
-			# If child order succeeds at smaller quantities, sell
-			# at a higher quantity
-			elif (number_of_successes == SUCCESS_TOLERANCE and child_order_size < maximum_child_size):
-				child_order_size *= 2
-				number_of_successes = 0
+            # logic for handling failure to sell
+            if (order['avg_price'] == 0.0):
+                number_of_successes -= 1
+            else:
+                number_of_successes += 1
+                # update quantity if successful
+                quantity_to_sell -= order['qty']
+            
+            # Divide child order quantity into smaller chunks if
+            # we hit the failure tolerance
+            if (number_of_successes == FAILURE_TOLERANCE):
+                child_order_size /= 2
+                number_of_successes = 0
+            # If child order succeeds at smaller quantities, sell
+            # at a higher quantity
+            elif (number_of_successes == SUCCESS_TOLERANCE and child_order_size < maximum_child_size):
+                child_order_size *= 2
+                number_of_successes = 0
 
-			# sleep for N seconds before selling again
-			time.sleep(N)
+            # sleep for N seconds before selling again
+            time.sleep(N)
 
-		# DONE with while loop!
-		self.status = self.COMPLETED
-		self.save() # we forgot to save after setting self.success=True. The update() function solves this
+        # DONE with while loop!
+        self.status = self.COMPLETED
+        self.save() # we forgot to save after setting self.success=True. The update() function solves this
 
-	def __str__(self):
-		return str(self.id) + ": " + str(self.quantity) + " x " + self.stock_type + ' status:' + self.status
+    def __str__(self):
+        return str(self.id) + ": " + str(self.quantity) + " x " + self.stock_type + ' status:' + self.status
 
 @python_2_unicode_compatible
 class ChildOrder(models.Model):
-	parent_order = models.ForeignKey(ParentOrder, on_delete=models.CASCADE)
-	quantity = models.IntegerField(blank=False, default=0)
-	is_successful = models.BooleanField(blank=False, default=False)
-	price = models.FloatField(blank=False, default=0.0)
-	time_executed = models.DateTimeField(blank=False, auto_now_add=True)
+    parent_order = models.ForeignKey(ParentOrder, on_delete=models.CASCADE)
+    quantity = models.IntegerField(blank=False, default=0)
+    is_successful = models.BooleanField(blank=False, default=False)
+    price = models.FloatField(blank=False, default=0.0)
+    time_executed = models.DateTimeField(blank=False, auto_now_add=True)
 
-	def __str__(self):
-		return "id: " + str(self.id) + " quantity: " + str(self.quantity) + " price: " + str(self.price) + " successful? " + str(self.is_successful)
+    def __str__(self):
+        return "id: " + str(self.id) + " quantity: " + str(self.quantity) + " price: " + str(self.price) + " successful? " + str(self.is_successful)
--- stocks/views.py
+++ stocks/views.py
@@ -13,124 +13,118 @@ from threading import Thread
 
 @login_required(login_url="/")
 def index(request):
-	if request.method == 'POST':
-		form = ParentOrderForm(request.POST)
+    if request.method == 'POST':
+        form = ParentOrderForm(request.POST)
 
-		if form.is_valid():
-			try:
-				order = ParentOrder(
-					quantity = form.cleaned_data['quantity'], 
-					stock_type = "ACME ETF", 
-					is_sell = True,
-					user = request.user)
-				if order.is_valid():
-					order.save()
-					api_response = order.query_market_datetime()
-					order.time_executed = api_response
-					order.save()  #second time is to save timestamp
+        if form.is_valid():
+            try:
+                order = ParentOrder(
+                    quantity = form.cleaned_data['quantity'], 
+                    stock_type = "ACME ETF", 
+                    is_sell = True,
+                    user = request.user)
+                if order.is_valid():
+                    order.save()
+                    api_response = order.query_market_datetime()
+                    order.time_executed = api_response
+                    order.save()  #second time is to save timestamp
 
-					t1 = Thread(target=order.trade)  # automatically try to execute trade upon submission
-					t1.daemon = True
-					t1.start()
-					return redirect('index')
+                    t1 = Thread(target=order.trade)  # automatically try to execute trade upon submission
+                    t1.daemon = True
+                    t1.start()
+                    return redirect('index')
 
-				else:
-					form.add_error('quantity', 'Enter a whole number greater than zero.')
-					# return HttpResponse("Invalid Order! Order quantity must be an integer greater than zero.")
+                else:
+                    form.add_error('quantity', 'Enter a whole number greater than zero.')
 
-			except:
-				form.add_error('quantity', '{:,d} is too large.'.format(form.cleaned_data['quantity']))
-				# return HttpResponse("Invalid Order!")
+            except:
+                form.add_error('quantity', '{:,d} is too large.'.format(form.cleaned_data['quantity']))
 
-	else:
-		form = ParentOrderForm()
+    else:
+        form = ParentOrderForm()
 
-	return render(request, 'place_order.html', {'form': form})
+    return render(request, 'place_order.html', {'form': form})
 
 @login_required(login_url="/")
 def order_detail(request, id):
-	if request.method == 'POST':
-		form = PauseResumeForm(request.POST)
+    if request.method == 'POST':
+        form = PauseResumeForm(request.POST)
 
-		if form.is_valid():
-			status_change = form.cleaned_data['status']
+        if form.is_valid():
+            status_change = form.cleaned_data['status']
 
-			if status_change == 'P':
-				resume_order(request, id)
-			elif status_change == 'S':
-				pause_order(request, id)
-			elif status_change == 'X':
-				cancel_order(request, id)
+            if status_change == 'P':
+                resume_order(request, id)
+            elif status_change == 'S':
+                pause_order(request, id)
+            elif status_change == 'X':
+                cancel_order(request, id)
 
-		else:
-			# TODO
-			return HttpResponse("Form not Valid")
+    form = PauseResumeForm()
 
-	form = PauseResumeForm()
+    parent = get_object_or_404(ParentOrder, id=id)
 
-	parent = get_object_or_404(ParentOrder, id=id)
+    try:
+        children = ChildOrder.objects.filter(parent_order__id=id).order_by('-id')
+    except ChildOrder.DoesNotExist:
+        children = ChildOrder.objects.none()
 
-	try:
-		children = ChildOrder.objects.filter(parent_order__id=id).order_by('-id')
-	except ChildOrder.DoesNotExist:
-		children = ChildOrder.objects.none()
+    # total price, total sold, and average price
+    parent_stats = parent.get_stats(children)
 
-	# total price, total sold, and average price
-	parent_stats = parent.get_stats(children)
-
-	return render(request, 'order_detail.html', 
-		{
-		'child_orders': children, 
-		'parent_order': parent, 
-		'total_price': '{:,.2f}'.format(parent_stats['total_price']), 
-		'total_sold': '{:,d}'.format(parent_stats['total_quantity']), 
-		'average_price': '{:,.2f}'.format(parent_stats['average_price']), 
-		'form': form
-		})
+    return render(request, 'order_detail.html', 
+        {
+        'child_orders': children, 
+        'parent_order': parent, 
+        'total_price': '{:,.2f}'.format(parent_stats['total_price']), 
+        'total_sold': '{:,d}'.format(parent_stats['total_quantity']), 
+        'average_price': '{:,.2f}'.format(parent_stats['average_price']), 
+        'form': form
+        })
 
 def get_progress(request, id):
-	parent = get_object_or_404(ParentOrder, id=id)
-	current_progress = '{:,.2f}'.format(parent.progress)
-	return HttpResponse(current_progress)
+    parent = get_object_or_404(ParentOrder, id=id)
+    current_progress = '{:,.2f}'.format(parent.progress)
+    return HttpResponse(current_progress)
 
 def get_status(request, id):
-	parent = get_object_or_404(ParentOrder, id=id)
-	return HttpResponse(parent.status)
+    parent = get_object_or_404(ParentOrder, id=id)
+    return HttpResponse(parent.status)
 
 def get_children(request, id, child_id):
-	parent = get_object_or_404(ParentOrder, id=id)
-	try:
-		children = ChildOrder.objects.filter(parent_order__id=id).filter(id__gt=child_id)
-	except ChildOrder.DoesNotExist:
-		children = ChildOrder.objects.none()
-	return HttpResponse(mark_safe(serialize('json', children)))
+    parent = get_object_or_404(ParentOrder, id=id)
+    try:
+        children = ChildOrder.objects.filter(parent_order__id=id).filter(id__gt=child_id)
+    except ChildOrder.DoesNotExist:
+        children = ChildOrder.objects.none()
+    return HttpResponse(mark_safe(serialize('json', children)))
 
 def get_children_undefined(request, id):
-	parent = get_object_or_404(ParentOrder, id=id)
-	try:
-		children = ChildOrder.objects.filter(parent_order__id=id)
-	except ChildOrder.DoesNotExist:
-		children = ChildOrder.objects.none()
-	return HttpResponse(mark_safe(serialize('json', children)))
+    parent = get_object_or_404(ParentOrder, id=id)
+    try:
+        children = ChildOrder.objects.filter(parent_order__id=id)
+    except ChildOrder.DoesNotExist:
+        children = ChildOrder.objects.none()
+    return HttpResponse(mark_safe(serialize('json', children)))
 
 def pause_order(request, id):
-	parent = get_object_or_404(ParentOrder, id=id)
-	parent.status = ParentOrder.PAUSED
-	parent.save()
+    parent = get_object_or_404(ParentOrder, id=id)
+    parent.status = ParentOrder.PAUSED
+    parent.save()
 
 def cancel_order(request, id):
-	parent = get_object_or_404(ParentOrder, id=id)
-	parent.status = ParentOrder.CANCELLED
-	parent.save()
+    parent = get_object_or_404(ParentOrder, id=id)
+    parent.status = ParentOrder.CANCELLED
+    parent.save()
 
 def resume_order(request, id):
-	parent = get_object_or_404(ParentOrder, id=id)
-	if(parent.status != ParentOrder.PAUSED):  # We can only resume orders that are paused
-		return False
-	parent.status = ParentOrder.IN_PROGRESS
-	parent.update_progress()
-	parent.save()
-	t1 = Thread(target=parent.trade)  # automatically try to execute trade upon submission
-	t1.daemon = True
-	t1.start()
-	return redirect('index')
+    parent = get_object_or_404(ParentOrder, id=id)
+    if(parent.status != ParentOrder.PAUSED):  # We can only resume orders that are paused
+        return False
+    parent.status = ParentOrder.IN_PROGRESS
+    parent.update_progress()
+    parent.save()
+    t1 = Thread(target=parent.trade)  # automatically try to execute trade upon submission
+    t1.daemon = True
+    t1.start()
+    return redirect('index')
